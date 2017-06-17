6f9568d92e510afc79cd4fd5105b22ee3e22d2d0
jaypogi69 <jayoliveros286@gmail.com>
2017-06-17 14:14:40 +0800

    added changes to baseline service

--- src/main/java/com/infor/dao/SlotsDao.java
+++ src/main/java/com/infor/dao/SlotsDao.java
@@ -1,5 +1,15 @@
 package com.infor.dao;
 
+import java.util.List;
+
+import com.infor.models.InforSlots;
+
 public interface SlotsDao {
+	public List<InforSlots> getUnassignedSlots();
+	public List<InforSlots> getAssignedSlots();
 	
+	/*
+	 * get all slots via result of @getUnassignedSlots and @getAssignedSlots
+	 */
+	public List<InforSlots> getAllSlots();
 }
---
+++ src/main/java/com/infor/idao/SlotsDaoImpl.java
@@ -0,0 +1,28 @@
+package com.infor.idao;
+
+import java.util.List;
+
+import com.infor.dao.SlotsDao;
+import com.infor.models.InforSlots;
+
+public class SlotsDaoImpl extends HibernateDaoSupport implements SlotsDao{
+
+	@Override
+	public List<InforSlots> getUnassignedSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InforSlots> getAssignedSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<InforSlots> getAllSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
