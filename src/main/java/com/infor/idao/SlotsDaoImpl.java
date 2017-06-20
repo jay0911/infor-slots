ecf15bca92b0bed9dd1a8f204c82d5bfafee6125
John Ryan Oliveros <johnryan.oliveros@infor.com>
2017-06-20 17:45:43 +0800

    baseline for slot module

--- src/main/java/com/infor/idao/SlotsDaoImpl.java
+++ src/main/java/com/infor/idao/SlotsDaoImpl.java
@@ -1,32 +1,68 @@
 package com.infor.idao;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import com.infor.dao.SlotsDao;
-import com.infor.models.InforSlots;
+import javax.transaction.Transactional;
 
+import org.springframework.stereotype.Repository;
+
+import com.infor.dao.SlotsDao;
+import com.infor.models.InforParking;
+import com.infor.models.InforSlots;
+import com.infor.models.InforUser;
+import com.infor.utils.ConverterUtils;
+
+@Repository
+@Transactional
 public class SlotsDaoImpl extends HibernateDaoSupport implements SlotsDao{
 
-	private final static String SELECT_ALL_SLOTS = "select * from tbl_inforparking ip inner join tbl_inforuser iu on ip.userid = iu.userid";
-	private final static String SELECT_UNASSIGNED_SLOTS = "from InforParking ip where ip.userid=0";
-	private final static String SELECT_ASSIGNED_SLOTS = "select * from tbl_inforparking ip inner join tbl_inforuser iu on ip.userid = iu.userid where ip.userid !=0";
+	private final static String SELECT_ALL_SLOTS = "select iu.userid,iu.firstname,iu.lastname,iu.contactnumber,iu.emailaddress,iu.inforaddress,iu.position,ip.parkingid,ip.isparkingtandem from tbl_inforparking ip left join tbl_inforuser iu on ip.userid = iu.userid";
+	private final static String SELECT_UNASSIGNED_SLOTS = "select iu.userid,iu.firstname,iu.lastname,iu.contactnumber,iu.emailaddress,iu.inforaddress,iu.position,ip.parkingid,ip.isparkingtandem from tbl_inforparking ip left join tbl_inforuser iu on ip.userid = iu.userid where ip.userid=0";
+	private final static String SELECT_ASSIGNED_SLOTS = "select iu.userid,iu.firstname,iu.lastname,iu.contactnumber,iu.emailaddress,iu.inforaddress,iu.position,ip.parkingid,ip.isparkingtandem from tbl_inforparking ip inner join tbl_inforuser iu on ip.userid = iu.userid where ip.userid !=0";
 	
 	@Override
 	public List<InforSlots> getUnassignedSlots() {
 		// TODO Auto-generated method stub
-		return null;
+		return commonSelectSlotsNative(SELECT_UNASSIGNED_SLOTS);
 	}
 
 	@Override
 	public List<InforSlots> getAssignedSlots() {
 		// TODO Auto-generated method stub
-		return null;
+		return commonSelectSlotsNative(SELECT_ASSIGNED_SLOTS);
 	}
 
 	@Override
 	public List<InforSlots> getAllSlots() {
 		// TODO Auto-generated method stub
-		return null;
+		return commonSelectSlotsNative(SELECT_ALL_SLOTS);
+	}
+	
+	@SuppressWarnings("unchecked")
+	private List<InforSlots> commonSelectSlotsNative(String query){
+		List<InforSlots> is = new ArrayList<InforSlots>();
+		List<Object[]> plainObj = customNativeSelectQuery(query).list();	
+		for(Object[] obj: plainObj){
+			InforUser inforUser = new InforUser();
+			InforParking inforParking = new InforParking();
+			InforSlots inforSlots = new InforSlots();
+			inforUser.setUserid(ConverterUtils.convertToInteger(obj[0]));
+			inforUser.setFirstname(ConverterUtils.convertToString(obj[1]));
+			inforUser.setLastname(ConverterUtils.convertToString(obj[2]));
+			inforUser.setContactnumber(ConverterUtils.convertToString(obj[3]));
+			inforUser.setEmailaddress(ConverterUtils.convertToString(obj[4]));
+			inforUser.setInforaddress(ConverterUtils.convertToString(obj[5]));
+			inforUser.setPosition(ConverterUtils.convertToString(obj[6]));
+			
+			inforParking.setParkingid(ConverterUtils.convertToString(obj[7]));
+			inforParking.setIsparkingtandem(ConverterUtils.convertToString(obj[8]));
+
+			inforSlots.setInforParking(inforParking);
+			inforSlots.setInforUser(inforUser);
+			is.add(inforSlots);
+		}	
+		return is;
 	}
 
 }
---
+++ src/main/java/com/infor/iservice/SlotsServiceImpl.java
@@ -0,0 +1,36 @@
+package com.infor.iservice;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.infor.dao.SlotsDao;
+import com.infor.models.InforSlots;
+import com.infor.service.SlotsService;
+
+@Service
+public class SlotsServiceImpl implements SlotsService{
+	
+	@Autowired
+	SlotsDao slotsDao;
+
+	@Override
+	public List<InforSlots> getUnassignedSlots() {
+		// TODO Auto-generated method stub
+		return slotsDao.getUnassignedSlots();
+	}
+
+	@Override
+	public List<InforSlots> getAssignedSlots() {
+		// TODO Auto-generated method stub
+		return slotsDao.getAssignedSlots();
+	}
+
+	@Override
+	public List<InforSlots> getAllSlots() {
+		// TODO Auto-generated method stub
+		return slotsDao.getAllSlots();
+	}
+
+}
